166c7d217a0197c0a519eb6d28d35e5370ba1c26
lauren.sciullo <lauren.sciullo@MU-C1-06.mullen.pvt.k12.co.us>
2019-09-11 11:57:29 -0600

    finished!

--- 2DArrayChallenge/src/ArrayChallenge2D.java
+++ 2DArrayChallenge/src/ArrayChallenge2D.java
@@ -4,6 +4,7 @@ public class ArrayChallenge2D
 
 		public static void main(String[] args)
 			{
+		System.out.println("Hello!");
 			
 		 int [ ] [ ] myArray = {{3, 5, 9, 2, -1, 8, 13}, 
 					 			{-2, 5, 1, 11, 8, 2, 0}, 
@@ -14,6 +15,8 @@ public class ArrayChallenge2D
 					 			{-3, 1, 1, 4, 11, 12, 4}};
 		 int sum = 0;
 		 int average = 0;
+		 int maxNumber = Integer.MIN_VALUE ;
+		 int minNumber = Integer.MAX_VALUE ;
 		 
 		 for (int row = 0; row < myArray.length; row++)
 			 {
@@ -21,13 +24,30 @@ public class ArrayChallenge2D
 					 {
 					 
 				sum += myArray[row][col];
-					 
+
 					 }
 			 }
 		
 		 average = sum / 49;
-		 System.out.println("This is your average: " + average);
-
-			}
-
+		 System.out.println("Your average is: " + average);
+		 
+		 for (int row = 0; row < myArray.length; row++)
+			 {
+				 for (int col = 0; col < myArray[0].length; col++)
+					 {
+						 if (myArray[row][col] > maxNumber)
+							 {
+								 maxNumber = myArray[row][col];
+							 }
+						 else if (myArray[row][col] < minNumber)
+							 {
+								 minNumber = myArray[row][col];
+							 }
+					 }
+			 }
+		 System.out.println("Your maximum number: " + maxNumber);
+		
+		
+		System.out.println("Your minimum number: " + minNumber);
+		}
 	}
